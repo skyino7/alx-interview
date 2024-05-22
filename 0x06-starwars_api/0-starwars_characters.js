cb0185b9e0caac1364a6a9277d686f228217a013
Seth Ketsi <christambassador12@gmail.com>
2024-05-22 19:50:47 +0000

    0. Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,2 +1,38 @@
 #!/usr/bin/node
 // Star Wars API
+
+const request = require('request');
+
+function fetchCharacters(filmId) {
+    const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+    request(url, async function (error, response, body) {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        const film = JSON.parse(body);
+        for (const characterUrl of film.characters) {
+            await new Promise((resolve, reject) => {
+                request(characterUrl, function (error, response, body) {
+                    if (error) {
+                        reject(error);
+                        return;
+                    }
+                    const character = JSON.parse(body);
+                    console.log(character.name);
+                    resolve();
+                });
+            });
+        }
+    });
+}
+
+// Extracting Film ID from command line arguments
+const filmId = process.argv[2];
+
+// Check if Film ID is provided
+if (filmId) {
+    fetchCharacters(filmId);
+} else {
+    console.log("Please provide the Movie ID as a command line argument.");
+}
