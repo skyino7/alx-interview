5000485dbce8dc5fabb372abd4b7cfbdedc5c53c
Seth Ketsi <christambassador12@gmail.com>
2024-05-29 21:56:21 +0000

    0. Rotate 2D Matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Rotate 2D Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotates a 2D matrix 90 degrees clockwise"""
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for row in range(n):
+        matrix[row].reverse()
